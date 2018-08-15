c41a83e779bb8b4f1bedac12c0b5662a0357fd96
atiwat.chananet <atiwat.chananet@gmail.com>
2018-08-15 23:00:42 +0700

    Add project files.

---
+++ Chana/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chana.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
